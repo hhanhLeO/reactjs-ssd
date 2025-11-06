0306fcc5095aafb3f295c75f13260d4f022dc99c
hhanhLeO <lthhanh04@gmail.com>
2025-11-06 22:07:58 +0700

    test reset api

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -6,7 +6,9 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { NotFoundPage } from './pages/NotFoundPage';
-import './App.css'
+import './App.css';
+
+window.axios = axios;
 
 function App() {
   const [cart, setCart] = useState([]);
